f943311238515ad73b9074216003d910271bb039
Tesfaye Hailu <thailu@LABA.PSWAS.LOCAL>
2023-06-28 13:05:51 -0700

    new veggieform

--- src/Page/veggie.jsx
+++ src/Page/veggie.jsx
@@ -1,11 +1,22 @@
 import axios from 'axios'
 
 import React, { useEffect, useState } from 'react'
-//show all veggies - this page will wait to get the veggie information from the backend ('/veggies'), when it gets the data, will display it on the page.
+
 const Veggie = () => {
     const [veggies, setVeggies]= useState([])
 
-    useEffect(async ()=>{
+    const [formData, setFormData] = useState({
+      name: '',
+      age: 1,
+      canEat: false
+    });
+
+    const [veggieArray, setVeggieArray] = useState([])
+
+    
+    //3.1 show all veggies - this page will wait to get the veggie information from the backend ('/veggies'), when it gets the data, will display it on the page.
+     
+    useEffect( ()=>{
 
         axios('/veggies').then((response)=> {
             console.log(response);
@@ -16,10 +27,68 @@ const Veggie = () => {
 
     }, [])
     console.log(veggies)
+
+    useEffect(() => {
+  
+      axios('/veggies').then((response)=> {
+        console.log(response);
+        setVeggieArray(response.data)
+      })
+  
+    }, []);
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(formData)
+      if (formData.age > 0) {
+        axios({
+           method: "POST",
+           url: "/veggies",
+           // goes to the server the app is from!
+           data: formData
+         })
+      } else {
+        // tell user negative values aren't allowed
+      }
+      // send form data to server create route
+    }
+  
+    const handleChange = (e) => {
+      let value = e.target.name === "canEat" ? e.target.checked : e.target.value;
+      let newStateObject = {
+        ...formData,
+        [e.target.name]: value
+      }
+      setFormData(newStateObject)
+    }
+  
+  
+    let veggieJSX = veggieArray.map((veggie) => {
+      return <div key={veggie._id} className={veggie.canEat ? "green" : "red"} >{veggie.name}</div>
+    });
+
+
+
   return (
     <div>
 
     <h2>Veggies</h2>
+
+    <form id="create-fruit-form" onSubmit={handleSubmit}>
+        <div className='input-container'>
+          <label htmlFor="name">veggie Name</label>
+          <input type="text" name="name" value={formData.name} onChange={handleChange}/>
+        </div>
+        <div className='input-container'>
+          <label htmlFor="age">veggie Age</label>
+          <input type="number" name="age" min="1" value={formData.age} onChange={handleChange}/>
+        </div>
+        <div className='input-container'>
+          <label htmlFor="canEat">Can Eat veggie?</label>
+          <input type="checkbox" name='canEat' checked={formData.canEat} onChange={handleChange}/>
+        </div>
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
